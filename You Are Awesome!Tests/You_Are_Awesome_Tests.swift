112d07a421630302b38faeda12c8f66d80566c3d
Morgan Prime <primem@bc.edu>
2020-09-07 14:21:29 -0400

    Initial Commit

---
+++ You Are Awesome!Tests/You_Are_Awesome_Tests.swift
@@ -0,0 +1,34 @@
+//
+//  You_Are_Awesome_Tests.swift
+//  You Are Awesome!Tests
+//
+//  Created by Morgan Prime on 9/7/20.
+//  Copyright © 2020 Morgan Prime. All rights reserved.
+//
+
+import XCTest
+@testable import You_Are_Awesome_
+
+class You_Are_Awesome_Tests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
